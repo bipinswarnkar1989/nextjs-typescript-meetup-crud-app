e4a3fd497b4d0a448260fa4cd7a7ba3fc9798726
bipinswarnkar1989 <vipinswarnkar1989@gmail.com>
2021-10-11 02:20:53 +0530

    chore: cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,25 +6,6 @@ import Head from 'next/head';
 import MeetupList from '../components/meetups/MeetupList';
 import { MeetupType } from '../components/meetups/types';
 
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 5, 12345 Some City',
-    description: 'This is a first meetup!',
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 10, 12345 Some City',
-    description: 'This is a second meetup!',
-  },
-];
-
 type Props = {
   meetups: MeetupType[];
 };
@@ -43,18 +24,6 @@ const HomePage: NextPage<Props> = ({ meetups }) => {
     </Fragment>
   );
 };
-
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   const req = context.req;
-//   const res = context.res;
-//   // fetch data from API
-
-//   return {
-//     props: {
-//       meetups: DUMMY_MEETUPS,
-//     },
-//   };
-// };
 
 export const getStaticProps: GetStaticProps = async (context) => {
   // fetch data from API
